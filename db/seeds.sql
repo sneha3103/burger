2692884b8ae55dc170d0bf746036c6815a72a165
Sneha Dama <snehadama@Snehas-Air.lan>
2018-04-30 12:30:09 -0500

    Added db information

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Veggie Patty Burger");
+INSERT INTO burgers (burger_name) VALUES ("Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
