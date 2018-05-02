b838a59c86871ddd828fcb00ba4402c0a618618b
Sneha Dama <snehadama@Snehas-Air.lan>
2018-05-02 14:08:56 -0500

    added routing to the burgers controller javascript file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,3 +6,22 @@ router.get("/", function(req, res){
     res.redirect("/burgers");
 
 });
+
+router.get("/burgers", function (req, res){
+    burgers.selectAll(function (data) {
+        var hbsObject = { burgers: data};
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.put("/burgers/update/:id", function (req, res){
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burgers.updateOne({ devoured: req.body.devoured }, condition, function() {
+        res.redirect("/burgers");
+    });
+});
+
+module.exports = router;
