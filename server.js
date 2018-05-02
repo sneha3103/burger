a263ca7e7f0f26da29744562900afefd4f15543b
Sneha Dama <snehadama@Snehas-Air.lan>
2018-05-02 13:29:17 -0500

    added body parser code and routes variable to server javascript file

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // Dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 var path = require("path");
+
+var methodOverride = require("method-override");
 
 
 // Create an instance of the express app.
@@ -15,14 +18,20 @@ var PORT = process.env.PORT || 8081;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(path.join(__dirname + "/public")));
 
+var routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
 
 
 app.get("/", function(req, res) {
     res.send("hi");
-    // res.render("index",  );
+    // res.render("index", {sql table name: data} );
   });
 
 
